b69b931e463063c68bbffd60f2bf7d21fe578d1d
DusterTheFirst <DusterTheFirst@gmial.com>
2016-10-02 10:59:54 -0400

    rremoved rong stuf

--- Locked Servers/src/test.java
+++
@@ -1,8 +0,0 @@
-import net.dusterthefirst.lockedServers.cryption.DualLayer;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println(DualLayer.encode("Um... Our Street Adress"));
-		System.out.println(DualLayer.decode(DualLayer.encode("Um... Our Street Adress")));
-	}
-}
