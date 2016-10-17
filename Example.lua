cec69abcc565efbca1ecba209f3c9d22be7ea0a0
DusterTheFirst <DusterTheFirst@users.noreply.github.com>
2016-10-17 08:09:54 -0400

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,22 @@
+Example syntax:
+
+Loops:
+For I in joe do
+End
+for I=0,I<12,I++ do
+end
+While true do
+End
+Repeate 
+Until true
+
+Conditionals:
+If true then
+End
+If true then
+Elseif false then
+Else
+End
+
+
+
